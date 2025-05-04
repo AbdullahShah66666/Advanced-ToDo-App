533e7d1b5e3e814e5a893afff44633e03c64c63b
Abdullah Shah <shah3152006@gmail.com>
2025-05-04 10:57:07 -0700

    First commit from new machine, improved the to do list

--- src/components/toDoList.tsx
+++ src/components/toDoList.tsx
@@ -1,6 +1,8 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRef, useState } from "react";
 import { FaMinusCircle, FaPlusCircle } from "react-icons/fa";
 
 interface ToDoItem {
@@ -13,6 +15,19 @@ export default function ToDoList() {
   [id: number]: boolean;
  }>({});
  const [toDoList, setToDoList] = useState<ToDoItem[]>([]);
+ const [triggerDeletion, setTriggerDeletion] = useState(false);
+ const inputRef = useRef<HTMLInputElement>(null);
+
+ const handleMinusClick = () => {
+  console.log("Minus icon clicked");
+  setTriggerDeletion((prev) => !prev);
+ };
+
+ const handleDeleteTask = () => {
+  console.log("Delete Task button clicked");
+  setToDoList((prev) => prev.filter((item) => !itemCheckedStates[item.id]));
+  setItemCheckedStates({});
+ };
 
  const handleCheckboxChange = (id: number, item: string) => {
   const newCheckedStates = { ...itemCheckedStates };
@@ -31,32 +46,62 @@ export default function ToDoList() {
  //  SampleList.push(newItem);
  // };
 
+ const addTask = (value: string) => {
+  if (!value.trim()) return;
+  const newItem: ToDoItem = { id: toDoList.length + 1, text: value.trim() };
+  setToDoList((prev) => [...prev, newItem]);
+ };
+
+ const handleAddTask = () => {
+  if (inputRef.current) {
+   addTask(inputRef.current.value);
+   inputRef.current.value = ""; // Clear the input field after adding the task
+  }
+  console.log("Add Button Clicked");
+ };
+
  const handleEnterKey = (e: React.KeyboardEvent<HTMLInputElement>) => {
   if (e.key === "Enter") {
+   addTask(e.currentTarget.value);
    console.log("Enter key pressed");
-   const inputValue = e.currentTarget.value.trim();
-   if (!inputValue) return;
-
-   const newItem: ToDoItem = { id: toDoList.length + 1, text: inputValue };
-
-   setToDoList((prev) => [...prev, newItem]);
    e.currentTarget.value = "";
   }
  };
 
+ const handleFocus = () => {
+  setTriggerDeletion(false);
+ };
+
  return (
-  <div className='bg-blue-500 flex flex-col items-center  gap-2 container max-w-auto'>
+  <div className='bg-blue-500 flex flex-col items-center gap-2 container max-w-auto min-h-screen'>
    {/* input for taking list items */}
-   <div className='bg-background text-foreground rounded-3xl relative w-1/3 has-focus-within:w-1/2 transition-width duration-300'>
-    <Input
-     className='rounded-3xl placeholder:text-sm placeholder:opacity-100 placeholder:text-foreground focus:placeholder:opacity-70 transition-all duration-300'
-     placeholder='Add A Task Here...'
-     //onChange={handleInputChange}
-     onKeyDown={handleEnterKey}
-     //     value={inputValue}
-    />
-    <FaMinusCircle className='absolute top-1/2 right-8 transform -translate-y-1/2' />
-    <FaPlusCircle className='absolute top-1/2 right-2 transform -translate-y-1/2' />
+   <div className='bg-background text-foreground rounded-3xl flex flex-row justify-center gap-1 w-3/4'>
+    <div className=' relative w-2/4 has-focus-within:w-full transition-width duration-600'>
+     <Input
+      ref={inputRef}
+      className='rounded-3xl placeholder:text-sm placeholder:opacity-100 placeholder:text-foreground focus:placeholder:opacity-70 transition-all duration-300'
+      placeholder='Add A Task Here...'
+      //onChange={handleInputChange}
+      onKeyDown={handleEnterKey}
+      onFocus={handleFocus}
+      //     value={inputValue}
+     />
+     <FaMinusCircle
+      onClick={handleMinusClick}
+      className='absolute top-1/2 right-2 transform -translate-y-1/2'
+     />
+    </div>
+    {triggerDeletion ? (
+     <Button variant={"destructive"} onClick={handleDeleteTask}>
+      <FaPlusCircle className='pt-0.5' />
+      <span>Delete Task</span>
+     </Button>
+    ) : (
+     <Button variant={"destructive"} onClick={handleAddTask}>
+      <FaPlusCircle className='pt-0.5' />
+      <span>Add Task</span>
+     </Button>
+    )}
    </div>
    {/* parent div of the ul to show list items */}
    <div className='w-5/12'>
@@ -67,13 +112,24 @@ export default function ToDoList() {
        className='odd:bg-green-600 even:bg-green-400 flex flex-row justify-between items-center gap-5 py-2 px-5  rounded-2xl'
       >
        <span>{item.text}</span>
-       <input
-        key={`${item.text}-checkbox`}
-        onChange={() => handleCheckboxChange(item.id, item.text)}
-        type='checkbox'
-        className='w-4 h-4'
-        checked={itemCheckedStates[item.id] || false}
-       />
+       <AnimatePresence>
+        {triggerDeletion && (
+         <motion.div
+          key={`checkbox-${item.id}`}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+         >
+          <input
+           onChange={() => handleCheckboxChange(item.id, item.text)}
+           type='checkbox'
+           className='w-4 h-4 accent-red-600'
+           checked={itemCheckedStates[item.id] || false}
+          />
+         </motion.div>
+        )}
+       </AnimatePresence>
       </li>
      ))}
     </ul>
